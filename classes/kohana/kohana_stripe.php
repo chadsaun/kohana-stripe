d6b06a611b2494a71da0c1623ac4188c057a8d65
Chad Saunders <chad@ifrogz.com>
2012-04-15 16:27:18 -0600

    Initial files

---
+++ classes/kohana/kohana_stripe.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+require_once Kohana::find_file('vendor', 'stripe-php-1.6.5/lib/Stripe.php');
+
+/**
+ * Image manipulation support. Allows images to be resized, cropped, etc.
+ *
+ * @package    Kohana/Image
+ * @category   Base
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2009 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+abstract class Kohana_Stripe extends Stripe_Vendor {}
---
+++ classes/stripe.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Stripe extends Kohana_Stripe {}
